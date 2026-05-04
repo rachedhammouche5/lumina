626e220c5504ac695576354933cc7ebf8e47b4d2
hammouche rached <rachedhammouche5@gmail.com>
2026-05-04 18:55:24 +0100

    added loading phase

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,13 @@
 import LoginPageView from "@/app/ui/auth/LoginPageView";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
     <main className="min-h-screen bg-slate-950 px-4 py-10 flex items-center justify-center">
       <div className="w-full max-w-2xl">
-        <LoginPageView />
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginPageView />
+        </Suspense>
       </div>
     </main>
   );
